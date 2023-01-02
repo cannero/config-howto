a1a10b0717cf6917aeeb373221995d2aaa114a05
cannero <serunmal@gmail.com>
2023-01-02 17:58:56 +0100

    commands, more podman

--- dockerfiles/scala/run_scala.cmd
+++ dockerfiles/scala/run_scala.cmd
@@ -1,1 +1,3 @@
-podman run -it --rm -p 12000:80 --volume ./:/usr/source --workdir /usr/source --name scala_sbt my-scala:1 /bin/bash
+@echo off
+REM create volume with 'podman volume create sbt_cache'
+podman run -it --rm -p 12000:80 --volume ./:/usr/source --workdir /usr/source --mount type=volume,source=sbt_cache,target=/root/.cache/coursier --name scala_sbt my-scala:1 /bin/bash
